7b3a9356abbbf963ac70888ded974f5af871231a
Twet-ta <rain.illusive@mail.ru>
2023-09-29 16:29:43 +0300

    2_goal

--- ml-project2/ml_project2/main.py
+++ ml-project2/ml_project2/main.py
@@ -1,32 +1,36 @@
-import numpy as np
-import pandas as pd
-
+# import numpy as np
+# import pandas as pd
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 iris = load_iris()
-X = iris['data']
-y = iris['target']
-names = iris['target_names']
-feature_names = iris['feature_names']
+X = iris["data"]
+y = iris["target"]
+names = iris["target_names"]
+feature_names = iris["feature_names"]
 
-# Scale data to have mean 0 and variance 1 
+# Scale data to have mean 0 and variance 1
 # which is importance for convergence of the neural network
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Split the data set into training and testing
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=2)
+    X_scaled, y, test_size=0.2, random_state=2
+)
 
 # Support vector machine algorithm
-from sklearn.svm import SVC
+
+
 svn = SVC()
 svn.fit(X_train, y_train)
 
 # Predict from the test dataset
 predictions = svn.predict(X_test)
 # Calculate the accuracy
-from sklearn.metrics import accuracy_score
+
+
 accuracy_score(y_test, predictions)
